144a6aaaccff630202162ecca0deaad5d7d60da7
Yancheng Wang <peter.yancheng.wang@gmail.com>
2023-03-20 13:33:24 -0700

    Add files via upload

--- search.py
+++ search.py
@@ -83,6 +83,63 @@ class SuperNet(nn.Module):
     def _get_flops_loss(self, gumbel_weights):
         return torch.matmul(self.flops_choices_normalized, gumbel_weights)
 
+class SuperNet_CS_LS(nn.Module):
+    def __init__(self, input_size, hidden_size_choices, layer_choices, num_classes):
+        super(SuperNet_CS_LS, self).__init__()
+        
+        max_hidden_size = max(hidden_size_choices)
+        num_choices_hidden = len(hidden_size_choices)
+        num_choices_layer = len(layer_choices)
+        
+        self.arch_params_hidden = torch.nn.Parameter(torch.ones(num_choices_hidden), requires_grad=True)
+        self.arch_params_layer = torch.nn.Parameter(torch.ones(num_choices_layer), requires_grad=True)
+        
+        self.masks = get_channel_mask(hidden_size_choices)
+        self.flops_choices = get_flops_choices(input_size, hidden_size_choices, num_classes)
+        self.flops_choices_normalized = torch.FloatTensor(self.flops_choices / np.max(self.flops_choices))
+        self.layer_choices_tensor = torch.FloatTensor(layer_choices)
+        
+        self.fc1 = nn.ModuleList()
+        for layer_num in layer_choices:
+            layers = []
+            layers.append(nn.Linear(input_size, max_hidden_size))
+            layers.append(nn.ReLU(inplace=True))
+            for i in range(layer_num-1):
+                layers.append(nn.Linear(max_hidden_size, max_hidden_size))
+                layers.append(nn.ReLU(inplace=True))
+            layers = nn.Sequential(*layers)
+        self.fc1.append(layers)
+        
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(max_hidden_size, num_classes)
+        
+    def forward(self, x, temperature):
+        out_fc1 = []
+        for fc1_each in self.fc1:
+            out_fc1_each = fc1_each(x)
+            out_fc1.append(out_fc1_each)
+        out = torch.stack(out_fc1, dim=-1)
+        
+        gumbel_weights_layer = gumbel_softmax(self.arch_params_layer, tau=temperature, hard=False)
+        out = torch.multiply(out, gumbel_weights_layer)
+        out = torch.sum(out,dim=-1)
+        # print(self.arch_params)
+        gumbel_weights_hidden = gumbel_softmax(self.arch_params_hidden, tau=temperature, hard=False)
+        # print(gumbel_weights)
+        mask = torch.multiply(self.masks, gumbel_weights_hidden)
+        mask = torch.sum(mask, dim=-1)
+        out = torch.multiply(out, mask)
+        
+        out = self.fc2(out)
+        flops_loss = self._get_flops_loss(gumbel_weights_layer, gumbel_weights_hidden)
+        return out, flops_loss
+    
+    def _get_flops_loss(self, gumbel_weights_layer, gumbel_weights_hidden):
+        layer_factor = torch.matmul(self.layer_choices_tensor, gumbel_weights_layer)
+        hidden_factor = torch.matmul(self.flops_choices_normalized, gumbel_weights_hidden)
+        flops = layer_factor * hidden_factor
+        return flops
+    
 # Initialize the neural network
 def search(x_train, y_train, out_dim, Loss_type='CE', hidden_size_choices = list(range(10,1000,10)), flops_balance_factor = 0.2, net_weight_lr = 0.0001, arch_lr = 0.01, num_epochs = 10000, search_freq = 20):
     # out_dim: The ouput dimension of the network. It should be the number of classes for 'CE' loss
@@ -147,5 +204,75 @@ def search(x_train, y_train, out_dim, Loss_type='CE', hidden_size_choices = list
                 print('Epoch [{}/{}], Overall_Loss: {:.4f}, CE_Loss: {:.4f}, Flops_Loss: {:.4f}, Accuracy: {:.2f}%, Seleted Channel {}'
                     .format(epoch+1, num_epochs, loss.item(), CE_loss.item(),
                             flops_loss.item(), accuracy * 100, selected_channel))
+    
+    return selected_channel
+
+
+# Initialize the neural network
+def search_CS_LS(x_train, y_train, out_dim, Loss_type='CE', hidden_size_choices = list(range(10,1000,10)),layer_choices = [1,2,3], flops_balance_factor = 0.2, net_weight_lr = 0.0001, arch_lr = 0.01, num_epochs = 10000, search_freq = 20):
+    # out_dim: The ouput dimension of the network. It should be the number of classes for 'CE' loss
+    # Loss_type: should be either 'CE' or 'MSE'
+    # hidden_size_choices: The choices for the number of hidden units.
+    # flops_balance_factor: The factor to balance performance (CE Loss or MSE Loss) and FLOPs. 
+    #                       Larger flops_balance_factor leads to a faster network with worse performance
+    # net_weight_lr: The learning rate for optimizing network weights 
+    # arch_lr: The learning rate for optimizing architecture parameters 
+    # num_epochs: Search Epochs
+    # search_freq: The algorithm teartively optimize architecture parameters and network weights every 'search_freq' epochs
+    
+    # The tempreature is decayed by 'temp_anneal_factor' every 'temp_anneal_freq' epochs
+    # Larger temp leads to a gumbel weight that is more close to 1-hot distribution  
+    temp = 5
+    temp_anneal_factor = 0.95
+    temp_decay_count = 25 # The temperatur will decay 'temp_decay_count' times in all during the search
+    temp_anneal_freq = num_epochs / temp_decay_count # The temperatur will decay every 'temp_anneal_freq' epochs
+
+    in_dim = x_train.shape[1]
+
+    net = SuperNet_CS_LS(in_dim, hidden_size_choices, layer_choices, out_dim)
+
+    # Set up the loss function and optimizer
+    if Loss_type == 'CE':
+        criterion = nn.CrossEntropyLoss()
+    elif Loss_type == 'MSE':
+        criterion = nn.MSELoss()
+
+    optimizer_net = optim.Adam([p for name, p in net.named_parameters() if 'arch' not in name], lr=net_weight_lr)
+    optimizer_arch = optim.Adam([p for name, p in net.named_parameters() if 'arch' in name], lr=arch_lr)
+    
+    warmup = 50
+
+    for epoch in range(num_epochs):
+        if (epoch % temp_anneal_freq == 0) and (epoch>=warmup):
+            temp = temp * temp_anneal_factor
+        
+        # Forward pass
+        outputs, flops_loss = net(x_train, temp)
+        CE_loss = criterion(outputs, y_train)
+        loss = (1 - flops_balance_factor) * CE_loss + flops_balance_factor * flops_loss
+        
+        if (int(epoch/search_freq)%2==0) or (epoch<=warmup):
+            optimizer_net.zero_grad()
+            loss.backward()
+            optimizer_net.step()
+        else:
+            optimizer_arch.zero_grad()
+            loss.backward()
+            optimizer_arch.step()
+        
+        selected_channel_id = np.argmax(net.arch_params.data.numpy())
+        selected_channel = hidden_size_choices[selected_channel_id]
+        selected_layer_id = np.argmax(net.arch_params_layer.data.numpy())
+        selected_layer = layer_choices[selected_layer_id]
+        with torch.no_grad():
+            _, predicted = torch.max(outputs, 1)
+            total = y_train.size(0)
+            correct = (predicted == y_train).sum().item()
+            accuracy = correct / total
+            if (epoch+1) % 10 == 0:
+                
+                 print('Epoch [{}/{}], Loss: {:.4f}, CE_Loss: {:.4f}, Flops_Loss: {:.4f}, Accuracy: {:.2f}%, Channel {}, Layer {}'
+                       .format(epoch+1, num_epochs, loss.item(), CE_loss.item(),
+                               flops_loss.item(), accuracy * 100, selected_channel, selected_layer))
     
     return selected_channel
